7dde2d5dbe58279064891816348b457407279388
dozukwang <dozukwang@gmail.com>
2022-02-24 12:38:08 +0900

    feat: axios 추가

--- src/utils/getMovieData.js
+++ src/utils/getMovieData.js
@@ -1,6 +1,8 @@
+const axios = require('axios')
+
 const url = `https://www.omdbapi.com?apikey=${process.env.API_KEY}&s=$`
 export async function fetchMovie(title) {
-    const res = await fetch(`${url}${title}`);
+    const res = await axios.get(`${url}${title}`);
     const data = res.json();
     return data;
 }
