98f727750d8515a59cfc29b0ee63a74a610e6972
dozukwang <dozukwang@gmail.com>
2022-02-24 13:44:54 +0900

    fix: typo 수정

--- src/functions/getMovieData.js
+++ src/functions/getMovieData.js
@@ -1,11 +1,9 @@
 const axios = require('axios')
 
 exports.handler = async function (event) {
-  const {httpMethod, queryStringPrarameters} = event
-  if (httpMethod === 'GET') {
-    const {keyword} = queryStringPrarameters
+  const {queryStringParameters} = event
+    const {keyword} = queryStringParameters
     const url = `https://www.omdbapi.com?apikey=${process.env.API_KEY}&s=$${keyword}`
     const res = await axios.get(url)
     return res
-  }
 }
