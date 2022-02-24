1a73689bbcfba8e88dde6b0266f29f91438de4ea
dozukwang <dozukwang@gmail.com>
2022-02-24 13:47:59 +0900

    fix: 정보요청 주소 오류 확인중

--- src/functions/getMovieData.js
+++ src/functions/getMovieData.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
 
 exports.handler = async function (event) {
+  console.log('실행됨')
+  console.log(event)
   const {queryStringParameters} = event
     const {keyword} = queryStringParameters
     const url = `https://www.omdbapi.com?apikey=${process.env.API_KEY}&s=$${keyword}`
