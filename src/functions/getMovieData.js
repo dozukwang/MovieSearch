6325501cb39f2b8126417d3db5a19bd337f8659e
dozukwang <dozukwang@gmail.com>
2022-02-24 13:30:22 +0900

    fix: title 오류 keyword로 수정

--- src/functions/getMovieData.js
+++ src/functions/getMovieData.js
@@ -3,8 +3,8 @@ const axios = require('axios')
 exports.handler = async function (event) {
   const {httpMethod, queryStringPrarameters} = event
   if (httpMethod === 'GET') {
-    const {title} = queryStringPrarameters
-    const url = `https://www.omdbapi.com?apikey=${process.env.API_KEY}&s=$${title}`
+    const {keyword} = queryStringPrarameters
+    const url = `https://www.omdbapi.com?apikey=${process.env.API_KEY}&s=$${keyword}`
     const res = await axios.get(url)
     return res
   }
