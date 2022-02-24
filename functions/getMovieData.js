4f8697b3604470d963bf1f49731c62ce7f66f38a
dozukwang <dozukwang@gmail.com>
2022-02-24 14:15:28 +0900

    fix: response 에러 확인

--- functions/getMovieData.js
+++ functions/getMovieData.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
 
 exports.handler = async function (event) {
-  const {keyword} = event.queryStringParameters
+  const {queryStringParameters} = event
+  const { keyword } = queryStringParameters
   const url = `https://www.omdbapi.com?apikey=${process.env.API_KEY}&s=$${keyword}`
   const res = await axios.get(url)
+  console.log(res)
   return {
     statusCode: 200,
     body: JSON.stringify(res.data)
--- src/main.js
+++ src/main.js
@@ -13,8 +13,9 @@ async function fetchMovie(keyword) {
   const payload = {keyword}
   try {
     const res = await axios.get('/.netlify/functions/getMovieData', {params: payload});
-    const data = res.body;
-    return data;
+    console.log("data", res)
+    // const data = res.body;
+    // return data;
   } catch (e) {
     console.error(e);
   }
